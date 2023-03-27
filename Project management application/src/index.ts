aa4c2a27cdf767d8de9f9da842fb00996ce0616d
darya-startsava <daryastartsava@gmail.com>
2023-03-28 01:52:45 +0200

    chore: read mongodb config from env
    chore: ignore env
    
    feat:  add database

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import { PORT } from './constants';
-
+import * as dotenv from 'dotenv';
 import * as serverService from './services/server.service';
 
+dotenv.config();
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(process.env.MONGO_DB as string);
+    
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
